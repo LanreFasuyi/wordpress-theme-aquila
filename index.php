88543125efe47cadf41edd76dc6627f42341203d
Lanre Fasuyi <fasuyi.ola@gmail.com>
2025-12-18 10:01:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Main Template file 
+ * 
+ * @packagee aquila
+ * 
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme</title>
+</head>
+<body>
+    
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, temporibus. Consequatur, nam sint numquam tenetur, praesentium illum dolore quaerat, optio odit labore hic inventore. Voluptas sequi totam voluptatibus quas sint.</h1>
+</body>
+</html>
